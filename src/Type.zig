77b97276a8ec7bea4253f1a802a3648f0077fc61
iambnlvn <iambnlvn@gmail.com>
2024-07-17 14:58:21 -0500

    test: Add tests for types

--- src/Type.zig
+++ src/Type.zig
@@ -101,3 +101,94 @@ pub const Pointer = struct {
     }
     // Todo: implement a function to get the type of the pointer
 };
+
+test "Integer.new initializes correctly" {
+    const bitCount: u16 = 64;
+    const signedness = Type.Integer.Signedness.Unsigned;
+    const expectedValue: u64 = (@as(u64, @intFromEnum(Type.ID.Integer)) << Type.ID.position) | (@as(u64, @intFromEnum(signedness)) << Type.Integer.Signedness.position) | bitCount;
+
+    const integer = Type.Integer.new(bitCount, signedness);
+
+    try std.testing.expectEqual(expectedValue, integer.value);
+}
+test "Integer.getBitCount returns correct value" {
+    const bitCount: u16 = 64;
+    const signedness = Type.Integer.Signedness.Unsigned;
+    const integer = Type.Integer.new(bitCount, signedness);
+
+    try std.testing.expectEqual(bitCount, Type.Integer.getBitCount(integer));
+}
+test "Integer.getSignedness returns correct value" {
+    const bitCount: u16 = 64;
+    const signedness = Type.Integer.Signedness.Unsigned;
+    const integer = Type.Integer.new(bitCount, signedness);
+
+    try std.testing.expectEqual(signedness, Type.Integer.getSignedness(integer));
+}
+
+test "Boolean initializes correctly" {
+    const expectedValue: u64 = (@as(u64, @intFromEnum(Type.ID.Integer)) << Type.ID.position) | (1 << Type.Integer.Signedness.position) | 1;
+
+    try std.testing.expectEqual(expectedValue, Type.Boolean.value);
+}
+test "Boolean is unsigned" {
+    try std.testing.expectEqual(Type.Integer.Signedness.Unsigned, Type.Integer.getSignedness(Type.Boolean));
+}
+test "Boolean is 1 bit" {
+    try std.testing.expectEqual(1, Type.Integer.getBitCount(Type.Boolean));
+}
+//Todo!: Fix this test
+// test "Function.new initializes correctly" {
+//     const idx: u64 = 1;
+//     const moduleIdx: u64 = 2;
+//     const expectedValue: u64 = (@as(u64, @intFromEnum(Type.ID.Function)) << Type.ID.position) | (moduleIdx << Type.Module.position) | idx;
+
+//     const function = Type.Function.new(idx, moduleIdx);
+
+//     try std.testing.expectEqual(expectedValue, function.value);
+// }
+// test "Function.append appends correctly" {
+//     var fnTypes = ArrayList(Type.Function).init(std.testing.allocator);
+//     defer fnTypes.deinit();
+
+//     const idx: u64 = 1;
+//     const moduleIdx: u64 = 2;
+//     const expectedValue: u64 = (@as(u64, @intFromEnum(Type.ID.Function)) << Type.ID.position) | (moduleIdx << Type.Module.position) | idx;
+
+//     const function = Type.Function.new(idx, moduleIdx);
+//     const appendedFunction = Type.Function.append(&fnTypes, function, moduleIdx);
+
+//     try std.testing.expectEqual(expectedValue, appendedFunction.value);
+//     try std.testing.expectEqual(1, fnTypes.items.len);
+// }
+
+test "Array.new initializes correctly" {
+    const idx: u64 = 1;
+    const moduleIdx: u64 = 2;
+    const expectedValue: u64 = (@as(u64, @intFromEnum(Type.ID.Array)) << Type.ID.position) | (moduleIdx << Type.Module.position) | idx;
+
+    const array = Type.Array.new(idx, moduleIdx);
+
+    try std.testing.expectEqual(expectedValue, array.value);
+}
+
+test "Struct.new initializes correctly" {
+    const idx: u64 = 1;
+    const moduleIdx: u64 = 2;
+    const expectedValue: u64 = (@as(u64, @intFromEnum(Type.ID.Structure)) << Type.ID.position) | (moduleIdx << Type.Module.position) | idx;
+
+    const s = Type.Struct.new(idx, moduleIdx);
+
+    try std.testing.expectEqual(expectedValue, s.value);
+}
+
+test "Pointer.new initializes correctly" {
+    const moduleIdx: u64 = 1;
+    const idx: u64 = 2;
+    const expectedValue: u64 = (@as(u64, @intFromEnum(Type.ID.Pointer)) << Type.ID.position) | (moduleIdx << Type.Module.position) | idx;
+
+    const pointer = Type.Pointer.new(idx, moduleIdx);
+
+    try std.testing.expectEqual(expectedValue, pointer.value);
+    try std.testing.expectEqual(8, Type.Pointer.size);
+}
