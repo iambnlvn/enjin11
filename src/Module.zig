2fd4f816b2cf53d539c10c454367cb090b98650b
iambnlvn <iambnlvn@gmail.com>
2024-07-19 12:51:31 -0500

    feat: Add Module struct for managing internal functions, literals, types, and libraries

---
+++ src/Module.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const ArrayList = @import("ArrayList.zig");
+
+const Function = @import("Parser.zig").Function;
+const Type = @import("Type.zig");
+const Parser = @import("Parser.zig");
+pub const Module = struct {
+    internalFns: []Function.Internal,
+    intLiterals: []Parser.IntegerLiteral,
+    structLiterals: []Parser.StructLiteral,
+    arrayLiterals: []Parser.ArrayLiteral,
+    unresolvedTypes: []Type.UnresolvedType,
+    sliceTypes: []Type.Slice,
+    fnTypes: Type.Function,
+    ArrayTypes: []Type.Array,
+    structTypes: []Type.Struct,
+    libNames: []([]const u8),
+
+    const Builder = struct {
+        internalFns: ArrayList(Function.Internal),
+        intLiterals: ArrayList(Parser.IntegerLiteral),
+        structLiterals: ArrayList(Parser.StructLiteral),
+        arrayLiterals: ArrayList(Parser.ArrayLiteral),
+        unresolvedTypes: ArrayList(Type.UnresolvedType),
+        sliceTypes: ArrayList(Type.Slice),
+        fnTypes: ArrayList(Type.Function),
+        structTypes: ArrayList(Type.Struct),
+        libNames: ArrayList([]const u8),
+        idx: u32,
+    };
+};
