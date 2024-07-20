7ca6058875073eded98ef5b24cc4b543fcaa78f5
iambnlvn <iambnlvn@gmail.com>
2024-07-20 17:21:57 -0500

    feat: Implement parsing methods for different expressions

--- src/ParserEngine.zig
+++ src/ParserEngine.zig
@@ -1,10 +1,14 @@
 const std = @import("std");
 const Lexer = @import("Lexer.zig");
-const Function = @import("Parser.zig").Function;
+const Parser = @import("Parser.zig");
+const Function = Parser.Function;
+const Precedence = Parser.Precedence;
 const TokenTypeMap = @import("Parser.zig").TokenTypeMap;
-const Module = @import("Module");
+const Module = @import("Module").Module;
 const Allocator = std.mem.Allocator;
-
+const Entity = @import("Entity.zig").Entity;
+const Operator = Lexer.Operator;
+const EntityID = @import("EntityID.zig").ID;
 pub const ParserEngine = struct {
     const Self = @This();
     const countersType = [std.enums.values(Lexer.Token).len]u32;
@@ -45,7 +49,135 @@ pub const ParserEngine = struct {
         self.lexer.counters[@intFromEnum(token)] += 1;
         self.lexer.nextIdx += 1;
     }
+
     fn isCorrectToken(self: *Self, comptime token: Lexer.Token) bool {
         return (token == self.lexer.tokens[self.lexer.nextIdx]);
+    }
+
+    fn getAndConsume(self: *Self, comptime token: Lexer.Token) TokenTypeMap[@intFromEnum(token)] {
+        self.consumeToken(token);
+        return self.getToken(token);
+    }
+
+    fn parseArithmeticExpr(self: *Self, leftExpr: Entity, operator: Operator.ID, precedence: Precedence) Entity {
+        const arithmeticId = switch (operator) {
+            .Plus => Parser.ArithmeticExpr.ID.Add,
+            .Minus => Parser.ArithmeticExpr.ID.Sub,
+            .Mul => Parser.ArithmeticExpr.ID.Mul,
+            .Div => Parser.ArithmeticExpr.ID.Div,
+            .Mod => Parser.ArithmeticExpr.ID.Mod,
+            else => std.debug.panic("Invalid operator"),
+        };
+
+        const rightExpr = self.parsePrecedence(@as(Precedence, @enumFromInt(@intFromEnum(precedence) + 1)));
+        const arithmeticExpr = Parser.ArithmeticExpr{
+            .id = arithmeticId,
+            .left = leftExpr,
+            .right = rightExpr,
+        };
+
+        const currentScopeIdx = self.fnBuilder.currentScope;
+        const currentScope = &self.fnBuilder.scopeBuilders.items[currentScopeIdx];
+
+        const arithmeticExprIdx = currentScope.ArithmeticExprs.items.len;
+        currentScope.ArithmeticExprs.append(arithmeticExpr) catch unreachable;
+
+        return Entity.new(arithmeticExprIdx, EntityID.Scope.ArithmeticExpr, self.fnBuilder.currentScope);
+    }
+
+    fn parsePrecedence(self: *Self, precedence: Precedence) Entity {
+        const prefix = self.lexer.tokens[self.lexer.nextIdx];
+
+        const leftExpr = blk: {
+            switch (prefix) {
+                .Identifier => break :blk self.parsePrefixIdentifier(),
+                .IntLiteral => {
+                    const signedness = false;
+                    const intValue = self.getAndConsume(.IntLiteral).value;
+                    break :blk Parser.IntegerLiteral.new(&self.moduleBuilder.intLiterals, intValue, signedness, self.moduleBuilder.idx);
+                },
+            }
+        };
+        return self.parseInfix(leftExpr, precedence);
+    }
+
+    fn parsePrefixIdentifier(self: *Self) Entity {
+        const name = self.getAndConsume(.Identifier).value;
+        return Parser.newIdentifierExpr(&self.fnBuilder, name);
+    }
+
+    fn parseInfix(self: *Self, precedence: Precedence, precedentLeftExpr: Entity) Entity {
+        var leftExpr = precedentLeftExpr;
+        var hasLessPrecedence = true;
+        while (hasLessPrecedence) {
+            const token = self.lexer.tokens[self.lexer.nextIdx];
+            const newPrecedence = switch (token) {
+                .Sign => signBlk: {
+                    const sign = self.getToken(.Sign);
+                    const signPrecedence = switch (sign.value) {
+                        '{', '}', ';', ',' => Precedence.None,
+                        else => std.debug.panic("Invalid precedence!"),
+                    };
+                    break :signBlk signPrecedence;
+                },
+                .Operator => operatorBlk: {
+                    const operator = self.getToken(.Operator);
+                    const operatorPrecedence = switch (operator.value) {
+                        .RightParen, .RightBracket => Precedence.None,
+                        .Plus, .Minus => Precedence.LightArithmetic,
+                        .Mul, .Div, .Mod => Precedence.HeavyArithmetic,
+                        .Equal, .GreaterThan, .LessThan, .GreaterThanOrEqual, .LessThanOrEqual, .NotEqual => Precedence.Comparison,
+                        .Assignment => Precedence.Assignment,
+                        .Declaration => Precedence.Declaration,
+                        .LeftParen, .LeftBracket => Precedence.Call,
+                        else => std.debug.panic("Invalid precedence!"),
+                    };
+                    break :operatorBlk operatorPrecedence;
+                },
+                else => std.debug.panic("Unimplemented token! {any}", .{token}),
+            };
+            hasLessPrecedence = @intFromEnum(precedence) <= @intFromEnum(newPrecedence);
+
+            if (hasLessPrecedence) {
+                const op = self.getAndConsume(.Operator).value;
+                leftExpr = switch (op) {
+                    .Equal, .GreaterThan, .LessThan, .GreaterThanOrEqual, .LessThanOrEqual, .NotEqual => {
+                        self.parseComparison(leftExpr, op, newPrecedence);
+                    },
+                    .Plus, .Minus, .Mul, .Div, .Mod => {
+                        self.parseArithmeticExpr(leftExpr, op, newPrecedence);
+                    },
+                    .Assignment => {
+                        self.parseAssignment(leftExpr, newPrecedence);
+                    },
+                    //Todo: Implement the rest of the operators
+                    else => std.debug.panic("Invalid operator! {any}", .{op}),
+                };
+            }
+        }
+        return leftExpr;
+    }
+
+    fn parseComparison(self: *Self, leftExpr: Entity, operator: Operator.ID, precedence: Precedence) Entity {
+        const compId = switch (operator) {
+            .Equal => Parser.Comparison.ID.Equal,
+            .GreaterThan => Parser.Comparison.ID.GreaterThan,
+            .LessThan => Parser.Comparison.ID.LessThan,
+            .GreaterThanOrEqual => Parser.Comparison.ID.GreaterThanOrEqual,
+            .LessThanOrEqual => Parser.Comparison.ID.LessThanOrEqual,
+            .NotEqual => Parser.Comparison.ID.NotEqual,
+            else => std.debug.panic("Invalid operator"),
+        };
+        const rightExpr = self.parsePrecedence(@as(Precedence, @enumFromInt(@intFromEnum(precedence) + 1)));
+        return Parser.Comparison.new(&self.fnBuilder, compId, leftExpr, rightExpr);
+    }
+
+    fn parseAssignment(self: *Self, leftExpr: Entity, precedence: Precedence) Entity {
+        const rightExpr = self.parsePrecedence(@as(Precedence, @enumFromInt(@intFromEnum(precedence) + 1)));
+        return Parser.Assignment.new(&self.fnBuilder, leftExpr, rightExpr);
+    }
+
+    fn parseExpr(self: *Self) Entity {
+        return self.parsePrecedence(Precedence.Assignment);
     }
 };
