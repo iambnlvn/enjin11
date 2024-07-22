842a0782b14e9a406057f73dc9056e105174fb94
iambnlvn <iambnlvn@gmail.com>
2024-07-22 06:03:19 -0500

    feat: Add LevelToArrayIdMap for mapping levels to array IDs in Entity.zig

--- src/Entity.zig
+++ src/Entity.zig
@@ -13,7 +13,21 @@ pub const Entity = packed struct {
 
     const arrayIdPosition = Level.position - @bitSizeOf(Level);
     const arrayIndexPosition = @bitSizeOf(u32);
+    const arrayIdEnumType = std.meta.Int(.unsigned, Level.position - arrayIdPosition);
 
+    const LevelToArrayIdMap = blk: {
+        const levels = std.enums.values(Level);
+        var arrayIds: [levels.len]type = undefined;
+        for (levels, 0..) |level, idx| {
+            arrayIds[idx] = switch (level) {
+                Level.builtin => Builtin,
+                Level.module => Module,
+                Level.global => Global,
+                Level.scope => Scope,
+            };
+        }
+        break :blk arrayIds;
+    };
     pub fn new(baseIdx: u64, comptime arrayId: anytype, arrayIdx: u64) Self {
         const level = comptime switch (@TypeOf(arrayId)) {
             Builtin => Level.builtin,
@@ -25,5 +39,10 @@ pub const Entity = packed struct {
         return Self{
             .value = (@as(Self.Type, @intCast(@intFromEnum(level))) << Level.position) | (@as(Self.Type, @intCast(@intFromEnum(arrayId))) << arrayIdPosition) | (arrayIdx << arrayIndexPosition) | baseIdx,
         };
+    }
+    pub fn getArrayId(self: Self, comptime level: Level) LevelToArrayIdMap[@intFromEnum(level)] {
+        if (level != .scope and self.getLevel() != .global) {
+            return @as(LevelToArrayIdMap[@intFromEnum(level)], @enumFromInt(@as(std.meta.Int(.unsigned, @bitSizeOf(LevelToArrayIdMap[@intFromEnum(level)])), @intCast((self.value & (std.math.maxInt(arrayIdEnumType) << arrayIdPosition)) >> arrayIdPosition))));
+        }
     }
 };
