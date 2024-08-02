58712af17bf8a9569572b7e3fa3f9e6d29de12c8
iambnlvn <iambnlvn@gmail.com>
2024-08-02 18:19:34 -0500

    feat: Add Sem module to handle parsing and storing semantic information

---
+++ src/Sem.zig
@@ -0,0 +1,27 @@
+const Parser = @import("Parser.zig");
+const Type = @import("Type.zig");
+pub const Result = struct {
+    functions: []Parser.Function.Internal,
+    external: External,
+    importedModules: []Parser.ImportedModule,
+
+    integerLiterals: []Parser.IntegerLiteral,
+    arrayLiterals: []Parser.ArrayLiteral,
+    structLiterals: []Parser.StructLiteral,
+
+    sliceTypes: []Type.Slice,
+    functionTypes: []Type.Function,
+    arrayTypes: []Type.Array,
+    structTypes: []Type.Struct,
+};
+
+pub const External = struct {
+    functions: []Parser.Function.External,
+    libNames: [][]const u8,
+    symNames: [][]const u8,
+    libs: []Library,
+
+    pub const Library = struct {
+        symbols: []u32,
+    };
+};
