47edf0d211736d905990f7afe9b916ce01c78f3d
iambnlvn <iambnlvn@gmail.com>
2024-08-18 18:12:23 -0500

    feat: Add block printing and implement formatRef for Ref formatting

--- src/Formatter.zig
+++ src/Formatter.zig
@@ -4,7 +4,10 @@ const ArrayList = std.ArrayList;
 const IR = @import("IR.zig");
 const Program = IR.Program;
 const Ref = IR.Ref;
-const Instruction = @import("Instruction.zig");
+const Constant = IR.Constant;
+const BasicBlock = IR.BasicBlock;
+const Function = IR.Function;
+const Instruction = @import("Instruction.zig").Instruction;
 const Type = @import("Type.zig");
 const Format = std.fmt.format;
 
@@ -136,17 +139,171 @@ pub const Formatter = struct {
 
             try Format(writer, ") #{}\n{c}\n", .{ funcIdx, '{' }) catch unreachable;
 
-            // TODO: Implement block printing
-            // for (blockPrinters.items) |blockPrinter| {
-            //     if (blockPrinter.id != 0xffffffff) {
-            //         try Format(writer, "{}:\n", .{blockPrinter.id}) catch unreachable;
-            //     }
+            for (blockPrinters.items) |blockPrinter| {
+                if (blockPrinter.id != 0xffffffff) {
+                    try Format(writer, "{}:\n", .{blockPrinter.id}) catch unreachable;
+                }
 
-            //     for (blockPrinter.instructions.items) |instructionPrinter| {
-            //         const instructionRef = instructionPrinter.ref;
-            //     }
-            // }
+                for (blockPrinter.instructions.items) |instructionPrinter| {
+                    const instructionRef = instructionPrinter.ref;
+                    const instructionId = Instruction.getId();
+                    const instructionStr = @tagName(instructionId);
+                    const instructionIdx = instructionRef.getIDX();
+
+                    if (instructionPrinter.id) |irId| {
+                        try Format(writer, "\t%{} = ", .{irId});
+                    } else {
+                        try writer.writeAll("\t");
+                    }
+                    try Format(writer, "{s} ", .{instructionStr});
+
+                    switch (instructionId) {
+                        .call => {
+                            const call = &self.builder.instructions.call.items[instructionIdx];
+                            const calleeReturnType = call.type.toString(self);
+                            const callee = call.callee;
+                            const calleeDeclaration = switch (callee.getId()) {
+                                .GlobalFunc => &self.builder.functionBuilders.items[callee.getIDX()].declaration,
+                                .ExternalFunc => &self.builder.external.functions[callee.getIDX()].declaration,
+                                else => std.debug.panic("Unknown Id ! {}", .{callee.getId()}),
+                            };
+                            try Format(writer, "{s} @{s}(", .{ calleeReturnType, calleeDeclaration.name });
+
+                            for (call.args, 0..) |arg, argIdx| {
+                                try self.formatRef(writer, func, arg, &slotTracker, calleeDeclaration.type.argTypes[argIdx]);
+                                if (argIdx < call.args.len - 1) {
+                                    try writer.writeAll(", ");
+                                }
+                            }
+                            try writer.writeAll(")");
+                        },
+                        .alloc => {
+                            const allocation = &self.builder.instructions.alloc.items[instructionIdx];
+                            try Format(writer, "{s}", .{allocation.baseType.toString(self)});
+                        },
+                        .store => {
+                            const store = &self.builder.instructions.store.items[instructionIdx];
+
+                            try self.formatRef(writer, func, store.value, &slotTracker, store.type);
+                            try writer.writeAll(", ");
+
+                            try self.formatRef(writer, func, store.pointer, &slotTracker, null);
+                        },
+                        .br => {
+                            const br = &self.builder.instructions.br.items[instructionIdx];
+                            if (br.condition) |condition| {
+                                try self.formatRef(writer, func, condition, &slotTracker, null);
+                                try writer.writeAll(", ");
+                                try self.formatRef(writer, func, BasicBlock.getRef(br.destBasicBlock), &slotTracker, null);
+                                try writer.writeAll(", ");
+                                try self.formatRef(writer, func, BasicBlock.getRef(br.falsyDestBlock.?), &slotTracker, null);
+                            } else {
+                                try self.formatRef(writer, func, BasicBlock.getRef(br.destBasicBlock), &slotTracker, null);
+                            }
+                        },
+                        .load => {
+                            const load = &self.builder.instructions.load.items[instructionRef.getIDX()];
+                            try format(writer, "{s}, ", .{load.type.toString(self)});
+                            try self.formatRef(writer, func, load.pointer, &slotTracker, load.type);
+                        },
+                        .icmp => {
+                            const icmp = &self.builder.instructions.icmp.items[instructionRef.getIDX()];
+                            try Format(writer, "{s}", .{@tagName(icmp.id)});
+                            try self.formatRef(writer, func, icmp.left, &slotTracker, null);
+                            try writer.writeAll(", ");
+                            try self.formatRef(writer, func, icmp.right, &slotTracker, null);
+                        },
+                        .ret => {
+                            const ret = &self.builder.instructions.ret.items[instructionRef.getIDX()];
+                            try self.formatRef(writer, func, ret.value, &slotTracker, ret.type);
+                        },
+                        .memCopy => {
+                            const memCopy = &self.builder.instructions.memCopy.items[instructionRef.getIDX()];
+                            try self.formatRef(writer, func, memCopy.destination, &slotTracker, null);
+                            try writer.writeAll(", ");
+
+                            try self.formatRef(writer, func, memCopy.source, &slotTracker, null);
+                            try writer.writeAll(", ");
+                            try Format(writer, "i64 {}", .{memCopy.size});
+                        },
+                        .getElPtr => {
+                            const gep = &self.builder.instructions.gep.items[instructionRef.getIDX()];
+                            try Format(writer, "inbounds {s}, ", .{gep.type.toString(self)});
+                            try self.formatRef(writer, func, gep.ptr, &slotTracker, null);
+                            try writer.writeAll(", ");
+                            for (gep.indices[0 .. gep.indices.len - 1]) |idx| {
+                                try Format(writer, "{}, ", .{idx});
+                            }
+                            try Format(writer, "{}", .{gep.indices[gep.indices.len - 1]});
+                        },
+                        else => {
+                            try Format(writer, "not implemented: {}\n", .{instructionId});
+                            unreachable;
+                        },
+                    }
+                    try writer.writeAll("\n");
+                }
+            }
             try writer.writeAll("}\n");
+        }
+    }
+
+    fn formatRef(self: *const Formatter, writer: anytype, currentFn: *Function.builder, ref: Ref, slotTracker: *SlotTracker, expectedType: ?Type) !void {
+        switch (ref.getId()) {
+            .Constant => {
+                switch (Constant.getId(ref)) {
+                    .Array => {
+                        try writer.writeAll("@arrayLitPlacerholder");
+                    },
+                    .@"struct" => {
+                        const literal = self.builder.structLiterals.items[ref.getIDX()];
+                        const refType = literal.type;
+                        try Format(writer, "{s} @structLiteralPlaceholder", .{refType.toString(self)});
+                    },
+                    .Int => {
+                        const typeStr = if (expectedType) |expType| expType.toString(self) else "s32";
+                        const literal = self.builder.integerLiterals.items[ref.getIDX()];
+                        try Format(writer, "{s} {}", .{ typeStr, literal.value });
+                    },
+                    else => std.debug.panic("Unknown or invalid constant Id {}", .{Constant.getId()}),
+                }
+            },
+            .Instruction => {
+                const idx = getIdx(writer, ref, slotTracker);
+                switch (Instruction.getId(ref)) {
+                    .alloc => {
+                        const allocation = &self.builder.instructions.alloc.items[ref.getIDX()];
+                        const allocTypeStr = allocation.ptrType.toString(self);
+
+                        try Format(writer, "{s} %{}", .{ allocTypeStr, idx });
+                    },
+                    .load => {
+                        const load = &self.builder.instructions.load.items[ref.getIDX()];
+                        const loadTypeStr = load.type.toString(self);
+
+                        try Format(writer, "{s} %{}", .{ loadTypeStr, idx });
+                    },
+                    .icmp => try Format(writer, "i1 %{}", .{idx}),
+                    .mul, .add, .sub => try Format(writer, "s32 %{}", .{idx}),
+                    .call => {
+                        const call = &self.builder.instructions.call.items[ref.getIDX()];
+                        try Format(writer, "{s} %{}", .{ call.type.toString(self), idx });
+                    },
+                    .getElPtr => {
+                        const gep = &self.builder.instructions.gep.items[ref.getIDX()];
+                        try Format(writer, "{s} %{}", .{ gep.type.toString(self), idx });
+                    },
+                    else => std.debug.panic("Unknown or invalid instruction id {}", .{Instruction.getId(ref)}),
+                }
+            },
+            .BasicBlock => try Format(writer, "label %{}", .{getIdx(writer, ref, slotTracker)}),
+            .arg => {
+                const idx = ref.getIDX();
+                const argType = currentFn.declaration.type.argTypes[idx];
+                try Format(writer, "{s} %{}", .{ argType.toString(self), idx });
+            },
+            .None => try writer.writeAll("void"),
+            else => std.debug.panic("Invalid id {}", .{ref.getId()}),
         }
     }
 };
