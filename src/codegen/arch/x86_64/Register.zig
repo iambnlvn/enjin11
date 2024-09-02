028d3aa6d601d79c95eb11f7624ba79cae8c45e2
iambnlvn <iambnlvn@gmail.com>
2024-09-02 07:50:46 -0500

    refactor: Update Register to improve register allocation
    The Register has been refactored to improve the register allocation process. This includes implementing the `Allocator` struct with methods for allocating direct and indirect registers.

--- src/codegen/arch/x86_64/Register.zig
+++ src/codegen/arch/x86_64/Register.zig
@@ -130,5 +130,152 @@ const Register = extern union {
         args,
     };
 
-    // TODO: Implement Allocations
+    const Allocator = struct {
+        state: State,
+        argRegisters: []const Register.ID,
+
+        const Self = @This();
+
+        fn new(argRegisters: []const Register.ID) Self {
+            var res: Self = undefined;
+            res.argReigsters = argRegisters;
+            res.state.occupation.array = std.mem.zeroes([Register.count]occupationType);
+            return res;
+        }
+
+        fn free(self: *Self, reg: Register.ID) void {
+            self.state.occupation.array[@intFromEnum(reg)] = .None;
+        }
+
+        fn allocateDirect(self: *Self, value: IR.Ref, regSize: u8) Direct {
+            const registers = &self.state.registers.byType.legacy;
+            const occupation = &self.state.occupation.byType.legacy;
+
+            for (occupation, 0..) |*regOccupation, idx| {
+                if (regOccupation.* == .None) {
+                    regOccupation.* = .Direct;
+
+                    registers[idx].* = Register{
+                        .direct = Direct{
+                            .ref = value,
+                            .size = regSize,
+                            .modifier = 0,
+                            .register = @as(Register.ID, @enumFromInt(idx)),
+                        },
+                    };
+                    return registers[idx].direct;
+                }
+            }
+
+            std.debug.panic("Couldn't allocate register", .{});
+        }
+
+        fn allocateIndirect(self: *Self, ref: IR.Ref, offset: i32, size: u32) Indirect {
+            const registers = &self.state.registers.byType.legacy;
+            const occupation = &self.state.occupation.byType.legacy;
+
+            for (occupation, 0..) |*regOccupation, idx| {
+                if (regOccupation.* == .None) {
+                    regOccupation.* = .Indirect;
+
+                    registers[idx].* = Register{
+                        .indirect = Indirect{
+                            .ref = ref,
+                            .offset = offset,
+                            .alignment = size,
+                            .size = size,
+                            .register = @as(Register.ID, @enumFromInt(idx)),
+                        },
+                    };
+                    return registers[idx].indirect;
+                }
+            }
+
+            std.debug.panic("No free registers", .{});
+        }
+
+        // This seems to be a good approach to allocate registers but too much code for a basic functionality
+        // might check out how to pass functions as parameters in zig, if possible, I can pass a function to allocate a specific register
+
+        // fn allocateRegister(self: *Self, ref: IR.Ref, size: u32, kind: Occupation, offset: ?i32) void {
+        //     const registers = &self.state.registers.byType.legacy;
+        //     const occupation = &self.state.occupation.byType.legacy;
+
+        //     for (occupation, 0..) |*regOccupation, idx| {
+        //         if (regOccupation.* == .None) {
+        //             regOccupation.* = kind;
+        //             const regID = @as(Register.ID, @enumFromInt(idx));
+
+        //             if (kind == .Direct) {
+        //                 registers[idx].* = Register{
+        //                     .direct = Direct{
+        //                         .ref = ref,
+        //                         .size = @as(u8, size),
+        //                         .modifier = 0,
+        //                         .register = regID,
+        //                     },
+        //                 };
+        //             } else {
+        //                 registers[idx].* = Register{
+        //                     .indirect = Indirect{
+        //                         .ref = ref,
+        //                         .offset = offset.?,
+        //                         .alignment = size,
+        //                         .size = size,
+        //                         .register = regID,
+        //                     },
+        //                 };
+        //             }
+        //             return;
+        //         }
+        //     }
+
+        //     std.debug.panic("No free registers", .{});
+        // }
+
+        // fn allocateDirect(self: *Self, value: IR.Ref, regSize: u8) Direct {
+        //     self.allocateRegister(value, regSize, .Direct, null);
+        //     const registers = &self.state.registers.byType.legacy;
+        //     return registers[self.getLastAllocatedIndex()].direct;
+        // }
+
+        // fn allocateIndirect(self: *Self, ref: IR.Ref, offset: i32, size: u32) Indirect {
+        //     self.allocateRegister(ref, size, .Indirect, offset);
+        //     const registers = &self.state.registers.byType.legacy;
+        //     return registers[self.getLastAllocatedIndex()].indirect;
+        // }
+
+        // fn getLastAllocatedIndex(self: *Self) usize {
+        //     const occupation = &self.state.occupation.byType.legacy;
+        //     for (occupation, 0..) |*regOccupation, idx| {
+        //         if (regOccupation.* != .None) {
+        //             return idx;
+        //         }
+        //     }
+        //     std.debug.panic("No registers have been allocated", .{});
+        // }
+
+        fn allocateArg(self: *Self, argIdx: u32, size: u8) Direct {
+            const targetRegister = self.argRegisters[argIdx];
+            var occupation = &self.state.occupation.array[@intFromEnum(targetRegister)];
+
+            if (occupation == .None) {
+                occupation.* = .Direct;
+
+                var reg = &self.state.registers.array[@intFromEnum(targetRegister)];
+                reg.* = .{
+                    .direct = Direct{
+                        .ref = IR.Function.Arg.new(argIdx),
+                        .size = size,
+                        .modifier = 0,
+                        .register = targetRegister,
+                    },
+                };
+
+                return reg.direct;
+            }
+
+            std.debug.panic("Register already occupied or not ready for use", .{});
+        }
+    };
 };
